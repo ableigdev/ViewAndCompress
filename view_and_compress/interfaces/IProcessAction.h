4c66aae50088afc98265c3aafb58b3f3d3156e12
ableigdev <igor.yarovoy.98@ukr.net>
2019-01-28 15:09:04 +0200

    Add an interface for the compression and decompression process

---
+++ view_and_compress/interfaces/IProcessAction.h
@@ -0,0 +1,15 @@
+#ifndef IPROCESSACTION_H
+#define IPROCESSACTION_H
+
+namespace interfaces
+{
+    class IProcessAction
+    {
+    public:
+        IProcessAction() = default;
+        virtual ~IProcessAction() = default;
+        // TODO: Add methods
+    };
+}
+
+#endif // IPROCESSACTION_H
