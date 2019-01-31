a8a3ad6cfb10cae9ed32550e5b5ae7622794deb5
ableigdev <igor.yarovoy.98@ukr.net>
2019-01-31 21:57:02 +0200

    Deploy controller

--- view_and_compress/main.cpp
+++ view_and_compress/main.cpp
@@ -2,10 +2,17 @@
 #include <QLockFile>
 #include <QDir>
 #include <QMessageBox>
-#include "draco/compression/encode.h"
-#include "draco/core/cycle_timer.h"
-#include "draco/io/mesh_io.h"
-#include "draco/io/point_cloud_io.h"
+#include <memory>
+#include "interfaces/IProcess.h"
+#include "interfaces/IProcessFactory.h"
+#include "interfaces/IProcessType.h"
+#include "interfaces/IController.h"
+#include "interfaces/IViewer.h"
+#include "draco_implementation/DracoProcessFactory.h"
+#include "draco_implementation/DracoProcessImpl.h"
+#include "draco_implementation/DracoProcessTypeImpl.h"
+#include "gui/ViewerImpl.h"
+#include "controller/ControllerImpl.h"
 
 int main(int argc, char *argv[])
 {
@@ -21,6 +28,11 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    draco::EncoderBuffer buffer;
+    std::unique_ptr<interfaces::IProcessFactory> factory(new draco_impl::DracoProcessFactory);
+    std::unique_ptr<interfaces::IProcess> process(factory->createProcess());
+    std::unique_ptr<interfaces::IViewer> viewer(new ViewerImpl);
+    std::unique_ptr<interfaces::IController> controller(new controller::ControllerImpl(std::move(viewer), std::move(process)));
+    controller->show();
+
     return a.exec();
 }
