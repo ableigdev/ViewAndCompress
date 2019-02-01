d31414782d54c1fad588284a21a63a823c80bedf
ableigdev <igor.yarovoy.98@ukr.net>
2019-02-01 16:47:20 +0200

    Implement controller

--- view_and_compress/controller/ControllerImpl.cpp
+++ view_and_compress/controller/ControllerImpl.cpp
@@ -1,10 +1,14 @@
+#include <QMessageBox>
 #include "ControllerImpl.h"
+#include "draco_implementation/DracoCompressAction.h"
+#include "draco_implementation/DracoDecompressAction.h"
 
 controller::ControllerImpl::ControllerImpl(
         std::unique_ptr<interfaces::IViewer> viewer,
         std::unique_ptr<interfaces::IProcess> processModel)
     : m_Viewer(std::move(viewer)),
-      m_ProcessModel(std::move(processModel))
+      m_ProcessModel(std::move(processModel)),
+      m_ProcessAction(nullptr)
 {
     connect(m_Viewer.get(), &interfaces::IViewer::doCompress, this, &ControllerImpl::catchCompressionAction);
     connect(m_Viewer.get(), &interfaces::IViewer::doDecompress, this, &ControllerImpl::catchDecompressionAction);
@@ -17,10 +21,37 @@ void controller::ControllerImpl::show()
 
 void controller::ControllerImpl::catchCompressionAction()
 {
-
+    m_ProcessAction.reset(new draco_impl::DracoCompressAction);
+    helpDoAction();
 }
 
 void controller::ControllerImpl::catchDecompressionAction()
 {
+    m_ProcessAction.reset(new draco_impl::DracoDecompressAction);
+    helpDoAction();
+}
 
+void controller::ControllerImpl::helpDoAction()
+{
+    m_ProcessModel->setCompressionLevel(m_Viewer->getCompressionLevel());
+    m_ProcessModel->setQuantizationBits(m_Viewer->getQuantizationBits());
+    m_ProcessModel->setPathToSourceFile(m_Viewer->getPathToSourceFile());
+    m_ProcessModel->setPathToOutputFile(m_Viewer->getPathToOutputFile());
+    try
+    {
+        auto message = m_ProcessModel->doProcessAction(m_ProcessAction.get());
+        m_Viewer->message(message);
+    }
+    catch(const std::invalid_argument& e)
+    {
+        QMessageBox::critical(m_Viewer.get(), "Error!", e.what());
+    }
+    catch(const std::out_of_range& e)
+    {
+        QMessageBox::critical(m_Viewer.get(), "Error!", e.what());
+    }
+    catch(const std::runtime_error& e)
+    {
+        QMessageBox::critical(m_Viewer.get(), "Error!", e.what());
+    }
 }
--- view_and_compress/controller/ControllerImpl.h
+++ view_and_compress/controller/ControllerImpl.h
@@ -5,6 +5,7 @@
 #include "interfaces/IController.h"
 #include "interfaces/IProcess.h"
 #include "interfaces/IViewer.h"
+#include "interfaces/IProcessAction.h"
 
 namespace controller
 {
@@ -21,8 +22,12 @@ namespace controller
         virtual void catchDecompressionAction();
 
     private:
+        void helpDoAction();
+
+    private:
         std::unique_ptr<interfaces::IViewer> m_Viewer;
         std::unique_ptr<interfaces::IProcess> m_ProcessModel;
+        std::unique_ptr<interfaces::IProcessAction> m_ProcessAction;
     };
 }
 
